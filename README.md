c1b0036615845f2cc8c6b2bcb3ee153069488a03
PierreNedellec <142042693+PierreNedellec@users.noreply.github.com>
2026-09-18 18:09:09 +0100

    Playlist generated but only returns track ids not song titles.

---
+++ README.md
@@ -0,0 +1,4 @@
+This app will generate a playlist of your favourite tracks or of a particular genre which is a certain length of time. In this way, the playlist acts as a timer. This is more of a fun project than for solving a particular problem.
+
+Version 1: program can find a list of songs that have a total time close to the goal time out of a group of songs.
+Version 2: program can also take an input of genre or playlist
--- playlist_generator.py
+++ playlist_generator.py
@@ -2,8 +2,37 @@ import pandas as pd
 
 df = pd.read_csv("spotify-tracks.csv")
 
-sample = df.sample(n=100, random_state=42)
+target_min = int(input("How long should the playlist be in minutes? "))
+target_ms = target_min*60*1000
+#First version: random sample --> then selected to fill a determined time
+sample = df.sample(target_min*5)
 
-print(df.head())
-print(df.shape)
-print(df.dtypes)
+tracks = list(zip(sample["track_id"], sample["duration_ms"]))
+playlist = []
+
+def time(pl):
+    playlist_time = 0
+    for item in pl:
+        playlist_time += item[1]
+    return playlist_time
+
+while time(playlist) < target_ms:
+    playlist.append(tracks.pop(0))
+
+def shouldBeSwapped(pl,duration_target):
+    best = ['',-999999]
+    for item in pl:
+        if abs(item[1]-duration_target) < abs(best[1]-duration_target):
+            best = item
+    return best
+
+while time(playlist) != target_ms:
+    difference = time(playlist) - target_ms
+    next_track_length = tracks[0][1]
+    tracks.append(playlist.pop(playlist.index(shouldBeSwapped(playlist,next_track_length + difference))))
+    playlist.append(tracks.pop(0))
+    print(time(playlist)/60000)
+
+
+print(playlist)
+print(time(playlist)/60000)
